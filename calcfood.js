7f0c5e33d40980b38c229edf12c32367c38bee35
Muller Mendonça <mullerm.oficial@gmail.com>
2021-01-04 16:50:05 -0300

    ajustando função que soma os valores

--- calcfood.js
+++ calcfood.js
@@ -13,24 +13,22 @@
 
     const peopleValue = () =>{
       if(people.value === "" || people.value <= 0){
-       alert("Lamentamos, mas a o número de pessoas não pode ser inferior ou igual a zero") 
+       alert("Lamentamos, mas a o número de pessoas não pode ser inferior ou igual a zero!") 
       } else{
        finalSum();
       };
     }
     
      calc.addEventListener('click', peopleValue);
-     
 
     const finalSum = () =>{
-      result.style.display = 'block';
       const contribuir = 'Cada pessoa deverá contribuir com R$'
       const allFood =  +foods.value + +drinks.value + +sweets.value;
       const pay = (allFood / people.value).toFixed(2);
-      console.log(pay);
       if(allFood <= 0 || allFood === ''){
         alert('Por favor,  digite algum valor!')
       } else{
+        result.style.display = 'block';  
         result.innerText = contribuir + pay;
         clearValue();  
       }
